5a7e964a86a271c2813d7cfededd210cc4b09e7a
kaapyth0n <nikita.kapitonov@gmail.com>
2024-12-12 15:52:34 +0100

    Coins and Map Update

---
+++ Assets/Scripts/Money_Script.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money_Script : MonoBehaviour
+{
+    public int rotationSpeed = 70; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.left * Time.deltaTime * rotationSpeed);
+    }
+}
