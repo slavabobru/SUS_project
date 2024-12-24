1bb197d00de3ed6cedd610bb908d314351e15364
kaapyth0n <nikita.kapitonov@gmail.com>
2024-12-24 15:51:18 +0100

    score upd

--- Assets/Scripts/Enemy_Move_Script.cs
+++ Assets/Scripts/Enemy_Move_Script.cs
@@ -6,18 +6,24 @@ public class Enemy_Move_Script : MonoBehaviour
 {
     private Transform plPos;
     private Vector3 direction;
-    public float enSpeed = 3f;
-    public float attackRng = 3f;
+    public float enSpeed = 4f;
+    public float attackRng = 5f;
     private float plDistance;
 
     void Start()
     {
-        plPos = GameObject.Find("SUS");
+        plPos = GameObject.Find("SUS").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        plDistance = Vector3.Distance(transform.position, plPos.position);
+
+        if (plDistance <= attackRng)
+        {
+            direction = (plPos.position - transform.position).normalized;
+            transform.Translate(direction * Time.deltaTime * enSpeed);
+        }
     }
 }
--- Assets/Scripts/Score_Script.cs
+++ Assets/Scripts/Score_Script.cs
@@ -6,17 +6,19 @@ using TMPro;
 public class Score_Script : MonoBehaviour
 {
     public int ScoreCount;
-    public TextMeshGUI ScoreText;
+    public TextMeshProUGUI ScoreText;
     void Start()
     {
         ScoreCount = 0;
         ScoreText.text = "SCORE: " + ScoreCount.ToString();
+        print("1");
     }
 
-    private void OnCollizionEnter(Collizion collizion)
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("SmallCoin"))
         {
+            print("2");
             ScoreCount++;
             ScoreText.text = "SCORE: " + ScoreCount.ToString();
         }
